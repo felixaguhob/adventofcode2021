30528dfa6fb80f1bfccdcb5049997dd7191e8721
Felix Aguhob <felix.aguhob@opencastsoftware.com>
2021-12-07 11:02:22 +0000

    day 06 solution

---
+++ src/day06/FishTank.java
@@ -0,0 +1,48 @@
+package day06;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FishTank {
+
+    private long[] groupFishes = new long[9];
+    public FishTank(String initialFishes) {
+        String[] fishTimers = initialFishes.split(",");
+
+        for(String fishTimer : fishTimers) {
+            groupFishes[Integer.parseInt(fishTimer)] += 1;
+        }
+    }
+
+    public long getFishCount() {
+        long count = 0l;
+        for(long group : groupFishes) {
+            count += group;
+        }
+
+        return count;
+    }
+
+    public void runSimulation(int days) {
+        for (int i=1; i <= days; i++) {
+            long pregnantFishes = groupFishes[0];
+            for(int j=0; j <= groupFishes.length - 2; j++) {
+                groupFishes[j] = groupFishes[j + 1];
+            }
+            groupFishes[6] += pregnantFishes; //mommy fishies
+            groupFishes[8] = pregnantFishes; //offsrping fishies
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String drawNumbers = Files.readString(Paths.get("input_day06.txt"), StandardCharsets.US_ASCII);
+
+        FishTank fishTank = new FishTank(drawNumbers);
+
+        fishTank.runSimulation(256);
+
+        System.out.println("Total Fish in Tank After 256 Days: " + fishTank.getFishCount());
+    }
+}
---
+++ test/day06/FishTankTest.java
@@ -0,0 +1,47 @@
+package day06;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+class FishTankTest {
+    @Test
+    void testFishTankRunSimulation() {
+        String initialFish = "3,4,3,1,2";
+        FishTank fishTank = new FishTank(initialFish);
+
+        fishTank.runSimulation(2);
+    }
+
+    @Test
+    void testFishTankRunSimulation18Days() {
+        String initialFish = "3,4,3,1,2";
+        FishTank fishTank = new FishTank(initialFish);
+
+        fishTank.runSimulation(18);
+
+        assertEquals(26, fishTank.getFishCount());
+    }
+
+    @Test
+    void testFishTankRunSimulation80Days() {
+        String initialFish = "3,4,3,1,2";
+        FishTank fishTank = new FishTank(initialFish);
+
+        fishTank.runSimulation(80);
+
+        assertEquals(5934, fishTank.getFishCount());
+    }
+
+    @Test
+    void testFishTankRunSimulation256Days() {
+        String initialFish = "3,4,3,1,2";
+        FishTank fishTank = new FishTank(initialFish);
+
+        fishTank.runSimulation(256);
+
+        assertEquals(26984457539L, fishTank.getFishCount());
+    }
+}
